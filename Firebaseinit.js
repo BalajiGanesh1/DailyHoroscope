7b278d5f8d406dc64dd6139912898ac2532798ea
Balaji Ganesh <balaji.ganesh@psiog.com>
2019-02-01 18:39:18 +0530

    Sign in successful

---
+++ Firebaseinit.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyCTwM0Dhnr0cQBjJpyVltVrzejI2QsOKn8",
+    authDomain: "daily-horoscope-e4a39.firebaseapp.com",
+    databaseURL: "https://daily-horoscope-e4a39.firebaseio.com",
+    projectId: "daily-horoscope-e4a39",
+    storageBucket: "daily-horoscope-e4a39.appspot.com",
+    messagingSenderId: "17460378458"
+};
+firebase.initializeApp(config);
